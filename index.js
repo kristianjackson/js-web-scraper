7ad77ea76ec75ec5f157cb85f3bb3f9728e3a77f
Kristian Jackson <kristian.jackson@gmail.com>
2022-08-07 09:15:44 +0000

    Before first trial run

--- index.js
+++ index.js
@@ -6,4 +6,27 @@ const PORT = 8000
 
 const app = express()
 
+const url = "https://www.theguardian.com/uk"
+
+axios(url)
+    .then(response => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const articles = [];
+        $('.fc-item__title', html).each(function() {
+            const title = $(this).text()
+            const url = $(this).find('a').attr('href')
+            articles.push({
+                title,
+                url
+        })
+        })
+        console.log(articles)
+    }
+    )
+    .catch(error => {
+        console.log(error)
+    }
+    )
+    
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
